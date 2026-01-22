23f3d366bc92e28c6e7a959317214a1d06cee002
Nowroz Islam <116680399+nowroz@users.noreply.github.com>
2026-01-22 20:05:09 +0600

    Apply active state for navbar links

--- src/app/components/navBarLink/NavBarLink.jsx
+++ src/app/components/navBarLink/NavBarLink.jsx
@@ -1,11 +1,16 @@
+"use client"
+
 import Link from "next/link"
+import { usePathname } from "next/navigation"
 
 const NavBarLink = ({ navPath }) => {
   const { path, name } = navPath
 
+  const pathName = usePathname()
+
   return (
     <li>
-      <Link className="px-3 py-1 bg-amber-400 rounded-full text-black font-bold hover:bg-amber-500" href={path}>{name}</Link>
+      <Link className={`px-3 py-1 ${pathName === path ? "" : "bg-amber-400 text-black"} rounded-full font-bold hover:bg-amber-500 hover:text-black`} href={path}>{name}</Link>
     </li>
   )
 }
